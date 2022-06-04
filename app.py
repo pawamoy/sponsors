5e0f01d12380c4db45989096d3c891dd2be6b88a
Timothée Mazzucotelli <pawamoy@pm.me>
2022-06-04 16:36:08 +0200

    Try to fix git ops

--- app.py
+++ app.py
@@ -37,13 +37,13 @@ def push_update(payload):
     with tempfile.TemporaryDirectory() as tmpdir:
         logger.debug("Preparing SSH key and setting up git")
         tmpkey_path = Path(tmpdir, "deploy_key")
-        tmpkey_path.write_text(DEPLOY_KEY)
+        tmpkey_path.write_text(DEPLOY_KEY.strip())
         ssh_cmd = f"ssh -i {tmpkey_path} -oIdentitiesOnly=yes -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
         os.environ["GIT_SSH_COMMAND"] = ssh_cmd
 
         logger.debug("Cloning repository")
         repo_dir = Path(tmpdir, "sponsors")
-        run(["git", "clone", "git@github.com:pawamoy/sponsors", str(repo_dir)])
+        run(["git", "clone", "git@github.com:pawamoy/sponsors", str(repo_dir)], check=True)
 
         logger.debug("Updating history")
         history_file = repo_dir / "history.json"
@@ -56,5 +56,5 @@ def push_update(payload):
         payload_file.write_text(json.dumps(payload))
 
         logger.debug("Committing and pushing")
-        run(["git", "-C", str(repo_dir), "commit", "-am", "sponsors update"])
-        run(["git", "-C", str(repo_dir), "push"])
+        run(["git", "-C", str(repo_dir), "commit", "-am", "sponsors update"], check=True)
+        run(["git", "-C", str(repo_dir), "push"], check=True)
